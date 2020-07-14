3761cb4c528e93791f61723fa7dcaaac76030281
James Bourne <james.bourne@infinityworks.com>
2020-07-14 21:52:16 +0100

    Add Pino logs

--- handler.ts
+++ handler.ts
@@ -1,12 +1,23 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
-import 'source-map-support/register';
+import { APIGatewayProxyHandler } from "aws-lambda";
+import "source-map-support/register";
+import * as pino from "pino";
 
-export const hello: APIGatewayProxyHandler = async (event, _context) => {
+export const hello: APIGatewayProxyHandler = async (_event, _context) => {
+  const logger = pino();
+
+  logger.info("This is a log message from Pino!");
+
+  logger.warn("Just a warning...");
+
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
+    body: JSON.stringify(
+      {
+        message:
+          "Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!",
+      },
+      null,
+      2
+    ),
   };
-}
+};
