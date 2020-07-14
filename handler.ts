b634b6ec2fdd5e8f3def890266cbd1da1f7e75c8
James Bourne <james.bourne@infinityworks.com>
2020-07-14 22:58:03 +0100

    Add Pino console.log destination

--- handler.ts
+++ handler.ts
@@ -3,10 +3,20 @@ import "source-map-support/register";
 import * as pino from "pino";
 
 export const hello: APIGatewayProxyHandler = async (_event, _context) => {
-  const logger = pino();
+  const logger = pino(
+    {},
+    {
+      write(buffer: any, cb?: (err?: Error | null) => void) {
+        console.log("\n" + buffer);
+        if (cb) {
+          cb(null);
+        }
+        return true;
+      },
+    }
+  );
 
-  logger.info("This is a log message from Pino!");
-
+  logger.info("This is a log message from Pino, using console.log!");
   logger.warn("Just a warning...");
 
   return {
